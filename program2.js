296caa3fa7e98993e039a3764c7d93fe1845a99f
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2024-09-27 14:11:15 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,6 +3,10 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    if (s.length === 0) {
+        return 0;
+    }
+
     var romanMap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000};
     var result = 0;
 
