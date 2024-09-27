cf97fd50c4fcdfe74fc854a39d52a9719449e539
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2024-09-27 14:19:05 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,23 +1,2 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    var stack = [];
-    var mapping = {")": "(", "}": "{", "]": "["};
 
-    for (var i = 0; i < s.length; i++) {
-        if (s[i] in mapping) {
-            if (stack.length === 0 || mapping[s[i]] !== stack.pop()) {
-                return false;
-            }
-        } else {
-            stack.push(s[i]);
-        }
-    }
-
-    return stack.length === 0;
-};
-
-module.exports = { isValid };
 
