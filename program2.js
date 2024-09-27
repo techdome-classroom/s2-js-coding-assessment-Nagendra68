fbc488809948801c72607acc69849f21ddfdf889
Nagendra_bardahe <Nagendrabardahe.321@gmail.com>
2024-09-27 14:29:13 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,24 +3,26 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    if (s.length === 0) {
-        return 0;
+    if (!s) return 0;
+
+    const romanMap = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    };
+
+    let result = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        const current = romanMap[s[i]];
+        const next = romanMap[s[i + 1]];
+        result += current < next ? -current : current;
     }
 
-    var romanMap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000};
-    var result = 0;
-
-    for (var i = 0; i < s.length - 1; i++) {
-        if (romanMap[s[i]] < romanMap[s[i + 1]]) {
-            result -= romanMap[s[i]];
-        } else {
-            result += romanMap[s[i]];
-        }
-    }
-
-    result += romanMap[s[s.length - 1]]; // add the last numeral
-
-    return result;
+    return result + romanMap[s[s.length - 1]];
 };
 
 module.exports = { romanToInt };
